631a6dd20205c7cea1fd1f8a070f50543425f837
JohnSchweden <yevgen.schweden@hotmail.com>
2025-08-24 23:08:42 +0200

    chore: update dependencies and _app.tsx

--- apps/next/pages/_app.tsx
+++ apps/next/pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from 'next/app'
 import { NextTamaguiProvider } from 'app/provider/NextTamaguiProvider'
 import { appWithTranslation } from 'next-i18next'
+import { QueryProvider } from 'app/provider/QueryProvider'
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <NextTamaguiProvider locale={pageProps.locale}>
-      <Component {...pageProps} />
+      <QueryProvider>
+        <Component {...pageProps} />
+      </QueryProvider>
     </NextTamaguiProvider>
   )
 }
